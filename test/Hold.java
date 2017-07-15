488d4ed775f0513f282edf6bb1150dcc583a00e0
Sidi Mohamed <sm.cheikhahmed@gti.mr>
2017-07-15 06:42:55 -0500

    GREEN: add the getcopy method to the hold calss

--- test/Hold.java
+++ test/Hold.java
@@ -1,8 +1,18 @@
 
 public class Hold {
 
+	Patron patron;
+	Copy copy;
+	
 	public Hold(Patron p, Copy c) {
 		// TODO Auto-generated constructor stub
+		patron = p;
+		copy = c;
+	}
+
+	public Object getCopy() {
+		// TODO Auto-generated method stub
+		return copy;
 	}
 
 }
