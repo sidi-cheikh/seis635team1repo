2245277e847b445b31c59e39b69bf545d38eb523
Sidi Mohamed <sm.cheikhahmed@gti.mr>
2017-07-15 05:30:59 -0500

    RED: can mark hold

--- test/HoldTest.java
+++ test/HoldTest.java
@@ -11,5 +11,12 @@ public class HoldTest {
 		Patron p = new Patron("P1", "TestHod");
 		assertFalse(p.hasHold());
 	}
+	
+	@Test
+	public void canMarkAHold(){
+		Patron p = new Patron("P1", "TestHod");
+		p.markHold();
+		
+	}
 
 }
