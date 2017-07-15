6032e4e17e193c48de4068a4a9c3ef493e61f59e
Sidi Mohamed <sm.cheikhahmed@gti.mr>
2017-07-15 05:35:59 -0500

    BLUE: moving the code that create a p patron to a method

--- test/HoldTest.java
+++ test/HoldTest.java
@@ -6,15 +6,19 @@ import org.junit.Test;
 
 public class HoldTest {
 	
+	public Patron createNewPatron(){
+		return new Patron("P1", "TestHod");
+	}
+	
 	@Test
 	public void canCheckIfTheHoldInitiallySetCorrectly(){
-		Patron p = new Patron("P1", "TestHod");
+		Patron p = createNewPatron();
 		assertFalse(p.hasHold());
 	}
 	
 	@Test
 	public void canMarkAHold(){
-		Patron p = new Patron("P1", "TestHod");
+		Patron p = createNewPatron();
 		p.markHold();
 		
 	}
